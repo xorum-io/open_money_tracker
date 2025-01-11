c20f9235dcb1204079bb406775b490aacf03f6be
Yev Kanivets <yevhenii.kanivets@gmail.com>
2025-01-11 16:55:20 +0100

    36 - Migrate ImportExportActivity to ViewBinding

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/external/ImportExportActivity.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/external/ImportExportActivity.java
@@ -3,17 +3,21 @@ package com.blogspot.e_kanivets.moneytracker.activity.external;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.appcompat.app.AlertDialog;
+
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 
 import com.blogspot.e_kanivets.moneytracker.R;
 import com.blogspot.e_kanivets.moneytracker.activity.base.BaseBackActivity;
 import com.blogspot.e_kanivets.moneytracker.controller.external.ExportController;
 import com.blogspot.e_kanivets.moneytracker.controller.external.ImportController;
+import com.blogspot.e_kanivets.moneytracker.databinding.ActivityImportExportBinding;
 import com.blogspot.e_kanivets.moneytracker.entity.data.Record;
 import com.blogspot.e_kanivets.moneytracker.util.CrashlyticsProxy;
 
@@ -24,8 +28,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import butterknife.BindView;
-import butterknife.OnClick;
 import timber.log.Timber;
 
 public class ImportExportActivity extends BaseBackActivity {
@@ -36,19 +38,28 @@ public class ImportExportActivity extends BaseBackActivity {
     @Inject
     ExportController exportController;
 
-    @BindView(R.id.et_import_data)
-    EditText etImportData;
+    private ActivityImportExportBinding binding;
 
     @Override
-    protected int getContentViewId() {
-        return R.layout.activity_import_export;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        binding = ActivityImportExportBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initData();
+        initToolbar();
+        initViews();
     }
 
-    @Override
-    protected boolean initData() {
-        boolean result = super.initData();
+    private void initViews() {
+        binding.btnImport.setOnClickListener(view -> importRecords());
+        binding.btnExport.setOnClickListener(view -> exportRecords());
+    }
+
+    private boolean initData() {
         getAppComponent().inject(ImportExportActivity.this);
-        return result;
+        return true;
     }
 
     @Override
@@ -59,14 +70,11 @@ public class ImportExportActivity extends BaseBackActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_help:
-                showHelp();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.action_help) {
+            showHelp();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     public void showHelp() {
@@ -78,10 +86,9 @@ public class ImportExportActivity extends BaseBackActivity {
                 .show();
     }
 
-    @OnClick(R.id.btn_import)
     public void importRecords() {
         CrashlyticsProxy.get().logButton("Import Records");
-        final String data = etImportData.getText().toString().trim();
+        final String data = binding.etImportData.getText().toString().trim();
 
         AsyncTask<Void, Void, Integer> importTask = new AsyncTask<Void, Void, Integer>() {
             @Override
@@ -107,7 +114,6 @@ public class ImportExportActivity extends BaseBackActivity {
         importTask.execute();
     }
 
-    @OnClick(R.id.btn_export)
     public void exportRecords() {
         CrashlyticsProxy.get().logButton("Export Records");
         List<String> records = exportController.getRecordsForExport(0, Long.MAX_VALUE);

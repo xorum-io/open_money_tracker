cf700f06ffb7ab5ed963274f6261e1a9e58d9c14
Yev Kanivets <yevhenii.kanivets@gmail.com>
2025-01-11 16:50:58 +0100

    36 - Migrate ChangeDateDialog to ViewBinding

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/ui/ChangeDateDialog.java
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/ui/ChangeDateDialog.java
@@ -3,29 +3,20 @@ package com.blogspot.e_kanivets.moneytracker.ui;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.DatePicker;
 
-import com.blogspot.e_kanivets.moneytracker.R;
+import androidx.annotation.Nullable;
+
+import com.blogspot.e_kanivets.moneytracker.databinding.DialogChangeDateBinding;
 
 import java.util.Calendar;
 import java.util.Date;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-/**
- * Created on 10/09/14.
- *
- * @author Evgenii Kanivets
- */
-public class ChangeDateDialog extends AlertDialog{
+public class ChangeDateDialog extends AlertDialog {
 
     private Date date;
     private OnDateChangedListener listener;
 
-    @BindView(R.id.datePicker)
-    DatePicker datePicker;
+    private DialogChangeDateBinding binding;
 
     public ChangeDateDialog(Context context, Date date, OnDateChangedListener listener) {
         super(context);
@@ -34,30 +25,27 @@ public class ChangeDateDialog extends AlertDialog{
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_change_date);
-        ButterKnife.bind(ChangeDateDialog.this);
+
+        binding = DialogChangeDateBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
 
-        datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
+        binding.datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
+        binding.bOk.setOnClickListener(view -> ok());
+        binding.bCancel.setOnClickListener(view -> dismiss());
     }
 
-    @OnClick(R.id.b_ok)
     public void ok() {
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, datePicker.getYear());
-        cal.set(Calendar.MONTH, datePicker.getMonth());
-        cal.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
+        cal.set(Calendar.YEAR, binding.datePicker.getYear());
+        cal.set(Calendar.MONTH, binding.datePicker.getMonth());
+        cal.set(Calendar.DAY_OF_MONTH, binding.datePicker.getDayOfMonth());
 
         listener.OnDataChanged(cal.getTime());
-        dismiss();
-    }
-
-    @OnClick(R.id.b_cancel)
-    public void cancel() {
         dismiss();
     }
 

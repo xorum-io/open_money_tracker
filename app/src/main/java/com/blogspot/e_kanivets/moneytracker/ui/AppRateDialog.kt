844b610e91d9e0555476092cff8f8c941512ab8c
Yev Kanivets <yevhenii.kanivets@gmail.com>
2025-05-29 14:12:34 +0200

    38 - Convert AppRateDialog.java to Kotlin

---
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/ui/AppRateDialog.kt
@@ -0,0 +1,55 @@
+package com.blogspot.e_kanivets.moneytracker.ui
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import com.blogspot.e_kanivets.moneytracker.MtApp
+import com.blogspot.e_kanivets.moneytracker.controller.PreferenceController
+import com.blogspot.e_kanivets.moneytracker.databinding.DialogRateBinding
+import javax.inject.Inject
+
+class AppRateDialog(context: Context) : AlertDialog(context) {
+    companion object {
+        private const val GP_MARKET = "market://details?id="
+    }
+
+    private val contextRef = context
+    private lateinit var binding: DialogRateBinding
+
+    @Inject
+    lateinit var preferenceController: PreferenceController
+
+    init {
+        MtApp.get().appComponent?.inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DialogRateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.yesButton.setOnClickListener { yes() }
+        binding.maybeButton.setOnClickListener { dismiss() }
+        binding.thanksButton.setOnClickListener { thanks() }
+    }
+
+    fun yes() {
+        contextRef.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(GP_MARKET + contextRef.packageName)))
+        preferenceController.appRated()
+        dismiss()
+    }
+
+    fun thanks() {
+        preferenceController.appRated()
+        dismiss()
+    }
+
+    override fun dismiss() {
+        try {
+            super.dismiss()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+} 

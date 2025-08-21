7208f435f4ce18e8441017000c2510367847ad71
Yev Kanivets <yevhenii.kanivets@gmail.com>
2025-08-22 00:08:47 +0300

    38 - Convert ExchangeRatesActivity to Kotlin

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/exchange_rate/ExchangeRatesActivity.kt
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/exchange_rate/ExchangeRatesActivity.kt
@@ -1,130 +1,113 @@
-package com.blogspot.e_kanivets.moneytracker.activity.exchange_rate;
+package com.blogspot.e_kanivets.moneytracker.activity.exchange_rate
 
-import android.content.Intent;
+import android.content.Intent
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.BaseAdapter
+import com.blogspot.e_kanivets.moneytracker.R
+import com.blogspot.e_kanivets.moneytracker.activity.base.BaseBackActivity
+import com.blogspot.e_kanivets.moneytracker.adapter.ExchangeRateAdapter
+import com.blogspot.e_kanivets.moneytracker.controller.data.ExchangeRateController
+import com.blogspot.e_kanivets.moneytracker.databinding.ActivityExchangeRatesBinding
+import com.blogspot.e_kanivets.moneytracker.entity.ExchangeRatePair
+import com.blogspot.e_kanivets.moneytracker.util.CrashlyticsProxy
+import com.blogspot.e_kanivets.moneytracker.util.ExchangeRatesSummarizer
+import java.util.Collections
+import javax.inject.Inject
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+class ExchangeRatesActivity : BaseBackActivity() {
 
-import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-
-import com.blogspot.e_kanivets.moneytracker.R;
-import com.blogspot.e_kanivets.moneytracker.activity.base.BaseBackActivity;
-import com.blogspot.e_kanivets.moneytracker.adapter.ExchangeRateAdapter;
-import com.blogspot.e_kanivets.moneytracker.controller.data.ExchangeRateController;
-import com.blogspot.e_kanivets.moneytracker.databinding.ActivityExchangeRatesBinding;
-import com.blogspot.e_kanivets.moneytracker.entity.ExchangeRatePair;
-import com.blogspot.e_kanivets.moneytracker.util.CrashlyticsProxy;
-import com.blogspot.e_kanivets.moneytracker.util.ExchangeRatesSummarizer;
-
-import java.util.Collections;
-import java.util.List;
-
-import javax.inject.Inject;
-
-public class ExchangeRatesActivity extends BaseBackActivity {
-    @SuppressWarnings("unused")
-    private static final String TAG = "ExchangeRatesActivity";
-
-    private static final int REQUEST_ADD_EXCHANGE_RATE = 1;
-
-    @Inject
-    ExchangeRateController rateController;
-
-    private List<ExchangeRatePair> exchangeRateList;
-
-    private ActivityExchangeRatesBinding binding;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityExchangeRatesBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        initData();
-        initToolbar();
-        initViews();
+    companion object {
+        private const val REQUEST_ADD_EXCHANGE_RATE = 1
     }
 
-    private boolean initData() {
-        getAppComponent().inject(ExchangeRatesActivity.this);
-        return true;
+    @Inject lateinit var rateController: ExchangeRateController
+
+    private var exchangeRateList: List<ExchangeRatePair> = emptyList()
+
+    private lateinit var binding: ActivityExchangeRatesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityExchangeRatesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initData()
+        initToolbar()
+        initViews()
     }
 
-    private void initViews() {
-        registerForContextMenu(binding.listView);
-        binding.btnAddExchangeRate.setOnClickListener(view -> addExchangeRate());
-        binding.listView.setOnItemClickListener((adapterView, view, i, l) -> addExchangeRateOnBaseOfExisted(i));
-
-        update();
+    private fun initData(): Boolean {
+        appComponent.inject(this)
+        return true
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.menu_exchange_rate, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-
-        if (item.getItemId() == R.id.delete) {
-            deleteExchangeRate(info.position);
-            return true;
+    private fun initViews() {
+        registerForContextMenu(binding.listView)
+        binding.btnAddExchangeRate.setOnClickListener { addExchangeRate() }
+        binding.listView.setOnItemClickListener { _, _, position, _ -> 
+            addExchangeRateOnBaseOfExisted(position) 
         }
 
-        return super.onContextItemSelected(item);
+        update()
     }
 
-    public void deleteExchangeRate(int position) {
-        CrashlyticsProxy.get().logButton("Delete Exchange Rate");
-        rateController.deleteExchangeRatePair(exchangeRateList.get(position));
-        update();
-        setResult(RESULT_OK);
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.menu_exchange_rate, menu)
     }
 
-    public void addExchangeRate() {
-        CrashlyticsProxy.get().logButton("Add Exchange Rate");
-        Intent intent = new Intent(ExchangeRatesActivity.this, AddExchangeRateActivity.class);
-        startActivityForResult(intent, REQUEST_ADD_EXCHANGE_RATE);
-    }
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
 
-    public void addExchangeRateOnBaseOfExisted(int position) {
-        CrashlyticsProxy.get().logButton("Edit Exchange Rate");
-        if (position < 0 || position >= exchangeRateList.size()) return;
-        Intent intent = new Intent(ExchangeRatesActivity.this, AddExchangeRateActivity.class);
-        intent.putExtra(AddExchangeRateActivity.KEY_EXCHANGE_RATE, exchangeRateList.get(position));
-        startActivityForResult(intent, REQUEST_ADD_EXCHANGE_RATE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == AppCompatActivity.RESULT_OK) {
-            switch (requestCode) {
-                case REQUEST_ADD_EXCHANGE_RATE:
-                    update();
-                    setResult(RESULT_OK);
-                    break;
-
-                default:
-                    break;
+        return when (item.itemId) {
+            R.id.delete -> {
+                deleteExchangeRate(info.position)
+                true
             }
+            else -> super.onContextItemSelected(item)
         }
     }
 
-    private void update() {
-        exchangeRateList = new ExchangeRatesSummarizer(rateController.readAll()).getPairedSummaryList();
-        Collections.reverse(exchangeRateList);
+    fun deleteExchangeRate(position: Int) {
+        CrashlyticsProxy.get().logButton("Delete Exchange Rate")
+        rateController.deleteExchangeRatePair(exchangeRateList[position])
+        update()
+        setResult(RESULT_OK)
+    }
 
-        binding.listView.setAdapter(new ExchangeRateAdapter(ExchangeRatesActivity.this, exchangeRateList));
-        ((BaseAdapter) binding.listView.getAdapter()).notifyDataSetChanged();
+    fun addExchangeRate() {
+        CrashlyticsProxy.get().logButton("Add Exchange Rate")
+        val intent = Intent(this, AddExchangeRateActivity::class.java)
+        startActivityForResult(intent, REQUEST_ADD_EXCHANGE_RATE)
+    }
+
+    fun addExchangeRateOnBaseOfExisted(position: Int) {
+        CrashlyticsProxy.get().logButton("Edit Exchange Rate")
+        if (position < 0 || position >= exchangeRateList.size) return
+        val intent = Intent(this, AddExchangeRateActivity::class.java)
+        intent.putExtra(AddExchangeRateActivity.KEY_EXCHANGE_RATE, exchangeRateList[position])
+        startActivityForResult(intent, REQUEST_ADD_EXCHANGE_RATE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_ADD_EXCHANGE_RATE) {
+            update()
+            setResult(RESULT_OK)
+        }
+    }
+
+    private fun update() {
+        exchangeRateList = ExchangeRatesSummarizer(rateController.readAll()).pairedSummaryList
+        Collections.reverse(exchangeRateList)
+
+        binding.listView.adapter = ExchangeRateAdapter(this, exchangeRateList)
+        (binding.listView.adapter as BaseAdapter).notifyDataSetChanged()
     }
 }

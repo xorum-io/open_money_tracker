fb6f0ffe0d5db9029c17ff2ae99e85ad60694386
Yev Kanivets <yevhenii.kanivets@gmail.com>
2026-03-19 22:08:25 +0100

    38 - Remove optional binding from GraphFragment

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/charts/fragment/GraphFragment.kt
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/activity/charts/fragment/GraphFragment.kt
@@ -11,45 +11,11 @@ import com.blogspot.e_kanivets.moneytracker.report.chart.IMonthReport
 import com.github.mikephil.charting.data.BarData
 
 class GraphFragment : Fragment() {
-    companion object {
-        private const val ARG_MONTH_REPORT = "arg_month_report"
-        private const val ARG_NO_DATA_TEXT = "arg_no_data_text"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param monthReport report for some period grouped by months.
-         * @return A new instance of fragment GraphFragment.
-         */
-        fun newInstance(monthReport: IMonthReport): GraphFragment {
-            val fragment = GraphFragment()
-            val args = Bundle()
-            args.putParcelable(ARG_MONTH_REPORT, monthReport)
-            fragment.arguments = args
-            return fragment
-        }
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param noDataText text that will be displayed in case of error.
-         * @return A new instance of fragment GraphFragment.
-         */
-        fun newInstance(noDataText: String): GraphFragment {
-            val fragment = GraphFragment()
-            val args = Bundle()
-            args.putString(ARG_NO_DATA_TEXT, noDataText)
-            fragment.arguments = args
-            return fragment
-        }
-    }
 
     private var monthReport: IMonthReport? = null
     private var noDataText: String? = null
 
-    private var binding: FragmentGraphBinding? = null
+    private lateinit var binding: FragmentGraphBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,26 +25,30 @@ class GraphFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                             savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentGraphBinding.inflate(inflater, container, false)
-        return binding?.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
         if (monthReport == null) {
-            binding?.barChart?.setNoDataText(noDataText)
+            binding.barChart.setNoDataText(noDataText)
         } else {
             monthReport?.let { report ->
                 val barChartConverter = BarChartConverter(requireContext(), report)
 
-                val barData = BarData(barChartConverter.xAxisValueList,
-                        barChartConverter.barDataSetList)
+                val barData = BarData(
+                    barChartConverter.xAxisValueList,
+                    barChartConverter.barDataSetList
+                )
                 barData.setDrawValues(false)
 
-                binding?.barChart?.apply {
+                binding.barChart.apply {
                     setData(barData)
                     setDescription(null)
                     setVisibleXRangeMinimum(8f)
@@ -88,6 +58,28 @@ class GraphFragment : Fragment() {
                     setHighlightPerTapEnabled(false)
                 }
             }
+        }
+    }
+
+    companion object {
+
+        private const val ARG_MONTH_REPORT = "arg_month_report"
+        private const val ARG_NO_DATA_TEXT = "arg_no_data_text"
+
+        fun newInstance(monthReport: IMonthReport): GraphFragment {
+            val fragment = GraphFragment()
+            val args = Bundle()
+            args.putParcelable(ARG_MONTH_REPORT, monthReport)
+            fragment.arguments = args
+            return fragment
+        }
+
+        fun newInstance(noDataText: String): GraphFragment {
+            val fragment = GraphFragment()
+            val args = Bundle()
+            args.putString(ARG_NO_DATA_TEXT, noDataText)
+            fragment.arguments = args
+            return fragment
         }
     }
 }

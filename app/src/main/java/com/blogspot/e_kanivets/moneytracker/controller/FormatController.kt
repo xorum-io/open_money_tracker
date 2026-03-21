b9856eeeb35369fb700f6cd518ad315e6af15070
Yev Kanivets <yevhenii.kanivets@gmail.com>
2026-03-21 22:34:58 +0100

    38 - Refactor FormatController

--- app/src/main/java/com/blogspot/e_kanivets/moneytracker/controller/FormatController.kt
+++ app/src/main/java/com/blogspot/e_kanivets/moneytracker/controller/FormatController.kt
@@ -3,19 +3,10 @@ package com.blogspot.e_kanivets.moneytracker.controller
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import kotlin.math.abs
 import kotlin.math.roundToInt
 
 class FormatController(private val preferenceController: PreferenceController) {
-
-    companion object {
-        const val PRECISION_MATH = "precision_math"
-        const val PRECISION_INT = "precision_int"
-        const val PRECISION_NONE = "precision_none"
-        private val shortDateFormat = SimpleDateFormat("yyyy-MM-dd")
-        private val fullDateFormat = SimpleDateFormat("d MMMM yyyy")
-        private val timeFormat = SimpleDateFormat("HH:mm")
-        private val dateAndTimeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
-    }
 
     fun formatAmount(amount: Double): String = when (preferenceController.readDisplayPrecision()) {
         PRECISION_MATH -> formatPrecisionMath(amount)
@@ -30,11 +21,11 @@ class FormatController(private val preferenceController: PreferenceController) {
 
     fun formatPrecisionNone(amount: Double): String = String.format(Locale.US, "%.2f", amount)
 
-    fun formatSignedAmount(amount: Double): String = (if (amount >= 0.0) "+ " else "- ") + formatAmount(kotlin.math.abs(amount))
+    fun formatSignedAmount(amount: Double): String = (if (amount >= 0.0) "+ " else "- ") + formatAmount(abs(amount))
 
-    fun formatIncome(amount: Double, currency: String): String = (if (amount >= 0) "+ " else "- ") + formatAmount(kotlin.math.abs(amount)) + " " + currency
+    fun formatIncome(amount: Double, currency: String): String = (if (amount >= 0) "+ " else "- ") + formatAmount(abs(amount)) + " " + currency
 
-    fun formatExpense(amount: Double, currency: String): String = (if (amount > 0) "+ " else "- ") + formatAmount(kotlin.math.abs(amount)) + " " + currency
+    fun formatExpense(amount: Double, currency: String): String = (if (amount > 0) "+ " else "- ") + formatAmount(abs(amount)) + " " + currency
 
     fun formatDateToNumber(timestamp: Long): String = shortDateFormat.format(Date(timestamp))
 
@@ -43,4 +34,14 @@ class FormatController(private val preferenceController: PreferenceController) {
     fun formatTime(timestamp: Long): String = timeFormat.format(Date(timestamp))
 
     fun formatDateAndTime(timestamp: Long): String = dateAndTimeFormat.format(Date(timestamp))
+
+    companion object {
+        const val PRECISION_MATH = "precision_math"
+        const val PRECISION_INT = "precision_int"
+        const val PRECISION_NONE = "precision_none"
+        private val shortDateFormat = SimpleDateFormat("yyyy-MM-dd")
+        private val fullDateFormat = SimpleDateFormat("d MMMM yyyy")
+        private val timeFormat = SimpleDateFormat("HH:mm")
+        private val dateAndTimeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
+    }
 }
